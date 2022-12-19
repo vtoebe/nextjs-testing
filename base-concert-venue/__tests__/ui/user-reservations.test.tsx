6a5c207e4923efaa65459a0cbfca9ddde443c188
Vitória Toebe <vrtoebe@gmail.com>
2022-12-19 00:03:13 -0300

    test: add test for user with no reservations

--- base-concert-venue/__tests__/ui/user-reservations.test.tsx
+++ base-concert-venue/__tests__/ui/user-reservations.test.tsx
@@ -2,10 +2,20 @@ import { render, screen } from "@testing-library/react";
 
 import { UserReservations } from "@/components/user/UserReservations";
 
-test("Should show purchase more tickets button when user has reservations", async () => {
-  render(<UserReservations userId={1} />);
-  const purchaseMoreTicketsButton = await screen.findByRole("button", {
-    name: /purchase more tickets/i,
+describe("User Reservations Component", () => {
+  test("Should show purchase more tickets button when user has reservations", async () => {
+    render(<UserReservations userId={1} />);
+    const purchaseMoreTicketsButton = await screen.findByRole("button", {
+      name: /purchase more tickets/i,
+    });
+    expect(purchaseMoreTicketsButton).toBeInTheDocument();
   });
-  expect(purchaseMoreTicketsButton).toBeInTheDocument();
+
+  test("Should show purchase tickets button when user has zero reservations", async () => {
+    render(<UserReservations userId={2} />);
+    const purchaseTicketsButton = await screen.findByRole("button", {
+      name: /purchase tickets/i,
+    });
+    expect(purchaseTicketsButton).toBeInTheDocument();
+  });
 });
