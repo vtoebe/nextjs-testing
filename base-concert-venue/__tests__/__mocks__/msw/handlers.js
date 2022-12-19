4e9a59c1ca60f5d875b3bfcac7be32b78137450f
Vitória Toebe <vrtoebe@gmail.com>
2022-12-19 00:10:25 -0300

    test: update msw handler for show to be dynamic

--- base-concert-venue/__tests__/__mocks__/msw/handlers.js
+++ base-concert-venue/__tests__/__mocks__/msw/handlers.js
@@ -6,8 +6,9 @@ import { fakeUserReservations } from "../fakeData/userReservations";
 // :showId - msw syntax
 export const handlers = [
   rest.get("http://localhost:3000/api/shows/:showId", async (req, res, ctx) => {
+    const { showId } = req.params;
     const { fakeShows } = await readFakeData();
-    return res(ctx.json({ show: fakeShows[0] }));
+    return res(ctx.json({ show: fakeShows[Number(showId)] }));
   }),
   rest.get(
     "http://localhost:3000/api/users/:userId/reservations",
