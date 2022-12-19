8821eb92c03f62845d817f8d6eca88ce83fe6e3c
Vitória Toebe <vrtoebe@gmail.com>
2022-12-18 23:57:01 -0300

    test: add handler for user reservations

--- base-concert-venue/__tests__/__mocks__/msw/handlers.js
+++ base-concert-venue/__tests__/__mocks__/msw/handlers.js
@@ -1,6 +1,7 @@
 import { rest } from "msw";
 
 import { readFakeData } from "../fakeData";
+import { fakeUserReservations } from "../fakeData/userReservations";
 
 // :showId - msw syntax
 export const handlers = [
@@ -8,4 +9,12 @@ export const handlers = [
     const { fakeShows } = await readFakeData();
     return res(ctx.json({ show: fakeShows[0] }));
   }),
+  rest.get(
+    "http://localhost:3000/api/users/:userId/reservations",
+    async (req, res, ctx) => {
+      const { userId } = req.params;
+      const userReservations = Number(userId) === 1 ? fakeUserReservations : [];
+      return res(ctx.json({ userReservations }));
+    }
+  ),
 ];
