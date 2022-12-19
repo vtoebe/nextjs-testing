2f889f43190f668681356d5da62440a068256753
Vitória Toebe <vrtoebe@gmail.com>
2022-12-18 23:05:24 -0300

    test: add handler for showId to msw

--- base-concert-venue/__tests__/__mocks__/msw/handlers.js
+++ base-concert-venue/__tests__/__mocks__/msw/handlers.js
@@ -1,3 +1,11 @@
 import { rest } from "msw";
 
-export const handlers = [];
+import { readFakeData } from "../fakeData";
+
+// :showId - msw syntax
+export const handlers = [
+  rest.get("http://localhost:3000/api/shows/:showId", async (req, res, ctx) => {
+    const { fakeShows } = await readFakeData();
+    return res(ctx.json({ show: fakeShows[0] }));
+  }),
+];
