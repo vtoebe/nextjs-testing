6e5cf263e743c2092376c754a701e1c4d4cd2483
Vitória Toebe <vrtoebe@gmail.com>
2022-12-19 00:10:43 -0300

    test: add test for sold out show

--- base-concert-venue/__tests__/ui/reservation.test.tsx
+++ base-concert-venue/__tests__/ui/reservation.test.tsx
@@ -2,8 +2,16 @@ import { render, screen } from "@testing-library/react";
 
 import { Reservation } from "@/components/reservations/Reservation";
 
-test("Should show the correct number of seats", async () => {
-  render(<Reservation showId={0} submitPurchase={jest.fn()} />);
-  const seatCountText = await screen.findByText(/10 seats left/i);
-  expect(seatCountText).toBeInTheDocument();
+describe("Reservation Component", () => {
+  test("Should show the correct number of seats", async () => {
+    render(<Reservation showId={0} submitPurchase={jest.fn()} />);
+    const seatCountText = await screen.findByText(/10 seats left/i);
+    expect(seatCountText).toBeInTheDocument();
+  });
+
+  test("Should show the show is sold out when there are no available seats", async () => {
+    render(<Reservation showId={1} submitPurchase={jest.fn()} />);
+    const soldOutMessage = await screen.findByText(/Show is sold out!/i);
+    expect(soldOutMessage).toBeInTheDocument();
+  });
 });
